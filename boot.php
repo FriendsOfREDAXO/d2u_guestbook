1c2678bc296bb9a8956f8446cadc51dece3ec205
Tobias Krais <tk@design-to-use.de>
2017-06-20 14:39:40 +0200

    move settings to role options

--- boot.php
+++ boot.php
@@ -1,5 +1,5 @@
 <?php
 if(rex::isBackend() && is_object(rex::getUser())) {
 	rex_perm::register('d2u_guestbook[]', rex_i18n::msg('d2u_guestbook_rights'));
-	rex_perm::register('d2u_guestbook[settings]', rex_i18n::msg('d2u_guestbook_rights_settings'));	
+	rex_perm::register('d2u_guestbook[settings]', rex_i18n::msg('d2u_guestbook_rights_settings'), rex_perm::OPTIONS);	
 }
