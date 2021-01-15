158f60651c8487f04cecd9cb6732480180c83b21
Tobias Krais <tk@design-to-use.de>
2021-01-15 16:02:10 +0100

    prepare version 1.1.0

--- pages/setup.php
+++ pages/setup.php
@@ -56,6 +56,10 @@ else if($tvsgb_available) {
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_guestbook" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.1.0-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.0.9:</p>
 <ul>
 	<li>Fehler in der Updatedatei hat die Module nicht aktualisiert und zu einem fatalen Fehler führen können.</li>
